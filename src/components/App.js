95050703b5a3ded00ff75256c4beb3f56e7d84f5
Irfan Khan <irfankhan501@outlook.com>
2020-11-12 13:16:08 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,62 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  useEffect(() => {
+    document.addEventListener("keydown", arrowKeyPressed);
+  });
+
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: 0,
+      top: 0,
+    });
+  };
+
+  const start = () => {
+    setRenderBall(true);
+  };
+
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    }
+    return (
+      <button onClick={start} className="start">
+        Start
+      </button>
+    );
+  };
+
+  const arrowKeyPressed = (e) => {
+    if (e.keyCode == 39 && renderBall) {
+      setX((prevState) => prevState + 5);
+      setBallPosition({ left: x });
+    }
+    if (e.keyCode == 37 && renderBall) {
+      setX((prevState) => prevState - 5);
+      setBallPosition({ left: x });
+    }
+    if (e.keyCode == 40 && renderBall) {
+      setY((prevState) => prevState + 5);
+      setBallPosition({ top: y });
+    }
+    if (e.keyCode == 38 && renderBall) {
+      setY((prevState) => prevState - 5);
+      setBallPosition({ top: y });
+    }
+  };
 
   return (
     <div className="playground">
